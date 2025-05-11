1422a2e8453a1637f37d63d49a8fd22305bd77fa
adarshaady17 <adarshsahu6236@gmail.com>
2025-05-11 15:16:53 +0530

    Add file

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,13 +1,17 @@
 import mongoose from "mongoose";
 
-const connectDB=async()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("Connect MongoDB");
-    }catch(error){
-        console.log(error);
+const connectDB = async () => {
+    try {
+      await mongoose.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      });
+      console.log('MongoDB Connected...');
+    } catch (err) {
+      console.error('Connection error:', err.message);
+      process.exit(1);
     }
-}
+  };
 
 export default connectDB;
 
