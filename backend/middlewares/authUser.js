4b6183b061404180f029f4de4fdeaa648011a25e
Adarsh sahu <161847570+adarshaady17@users.noreply.github.com>
2025-05-16 21:49:00 +0530

    Update authUser.js

--- backend/middlewares/authUser.js
+++ backend/middlewares/authUser.js
@@ -2,14 +2,15 @@ import jwt from "jsonwebtoken";
 
 const authUser = async (req, res, next) => {
   const { token } = req.cookies;
-
+  console.log(token);
   if (!token) {
     return res.json({ success: false, message: "Unauthorized" });
   }
 
   try {
-    const tokenDecode = jwt.verify(token, process.env.SECRET_KEY); 
-
+    const tokenDecode = jwt.verify(token, process.env.SECRET_KEY);
+    console.log(process.env.SECRET_KEY);
+    console.log(tokenDecode);
     if (tokenDecode.id) {
       req.user = { id: tokenDecode.id }; // This sets req.user.id
     } else {
